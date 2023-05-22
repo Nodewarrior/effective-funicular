0dcf9b3f5599232ddebd91e4e6b177c7423d03c0
akshay <akshaykumar.kataiah@wework.com>
2023-05-22 02:15:14 -0400

    form changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    name: 'Sanji Vinsmoke',
+    gender: 'male',
+    dateOfBirth: '1990-01-01',
+    timeOfBirth: '12:00 PM',
+    location: 'New York',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //handle submission here
+    console.log(formData);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form className="container" onSubmit={handleSubmit}>
+      <label htmlFor='name'>
+        Name:
+        <input type="text" id="name" value={formData.name} onChange={handleChange}/>
+      </label>
+      <label htmlFor='gender'>
+        Gender:
+        <select id="gender" value={formData.gender} onChange={handleChange}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+      </label>
+      <div className="birth">
+        <label htmlFor='date'>
+          Date of Birth:
+          <input type="text" id="date" />
+        </label>
+        <label htmlFor='time'>
+          Time of Birth:
+          <input type="text" id="time" />
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <label htmlFor='birthLocation'>
+        Birth Location:
+        <input type="text" id="birthLocation" value={formData.location} onChange={handleChange}/>
+      </label>
+      <label htmlFor="emailId">
+        Email: 
+        <input type="email" id="emailId" />
+      </label>
+      <label htmlFor="agree" className='conditions'>
+        <input type="checkbox" id="agree" />
+        <p>I agree to the terms and conditions</p>
+      </label>
+      <label htmlFor="submit">
+        <button type="submit" className='btn-grad'>Get Details</button>
+      </label>
+    </form>
   )
 }
 
